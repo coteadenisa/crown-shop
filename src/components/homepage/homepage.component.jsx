afffd6ba5661c64ce571dfddc8811290cb88936d
Denisa Cotea <dcotea@optaros.com>
2020-11-05 13:12:33 +0200

    first files for crown-shop

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <div className='menu'>
+            <div className='menu__item'>
+                <div className='menu__item__content'>
+                    <h1 className='menu__item__content__title'>Hats</h1>
+                    <span className='menu__item__content__subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu__item'>
+                <div className='menu__item__content'>
+                    <h1 className='menu__item__content__title'>Jackets</h1>
+                    <span className='menu__item__content__subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu__item'>
+                <div className='menu__item__content'>
+                    <h1 className='menu__item__content__title'>Womens</h1>
+                    <span className='menu__item__content__subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu__item'>
+                <div className='menu__item__content'>
+                    <h1 className='menu__item__content__title'>Mens</h1>
+                    <span className='menu__item__content__subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu__item'>
+                <div className='menu__item__content'>
+                    <h1 className='menu__item__content__title'>Hats</h1>
+                    <span className='menu__item__content__subtitle'>Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
